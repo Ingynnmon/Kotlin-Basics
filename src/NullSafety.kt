cd28b266931e3010439a45f7d706ab8d2c8ada1c
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-08 10:02:14 +0630

    finished NullSafety.kt

---
+++ src/NullSafety.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>){
+    var nullVal: String?=null
+    //var nullVal: String="hello"
+
+    fun returnNull(): String?{
+        return null
+    }
+
+   // var nullVal2 = returnNull()
+
+    if(nullVal !=null){
+        println("nullVal2.length")
+    }else println("it is null")
+
+//    var nullVal3= nullVal2!!.length
+  //  var nullVal4:String = returnNull() ?: "No Name"
+
+}
